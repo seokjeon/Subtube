855f3d0ed49faa52d9e3df6f11c0826963f7fa05
SeOk Jeon <jso4967@gmail.com>
2020-01-13 16:05:54 +0900

    [Update] translblock form post feature

--- routes/base.js
+++ routes/base.js
@@ -1,21 +1,39 @@
 const router = require('express').Router()
+const mongoose = require('mongoose')
 
 const Video = require('../models/Video')
+const TranslationBlock = require('../models/TranslationBlock')
 const SentenceBlock = require('../models/SentenceBlock')
 
 
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
 
-router.get('/', function(req, res){
+router.get('/', function (req, res) {
   console.log("hi I'm node js server")
   res.send("HI CLIENT I'M NODE JS")
 })
 
 
-router.get('/trans/:id', function(req, res){
+router.post('/trans/:id', function (req, res) {
+  const processed_eng = req.body.RawEng
+  const translated_kor = req.body.TranslatedKor
+  
+  //DB에 저장
+  TranslationBlock.create({
+    sentence_block_id: '5e1c15b747a4361fd416fef8',
+    processed_eng: processed_eng,
+    translated_kor: translated_kor,
+    num_of_votes: 4967
+  })
+  //아이콘 바꾸기
+
+  console.log(processed_eng + ' / ' + translated_kor)
+})
+
+router.get('/trans/:id', function (req, res) {
   videoId = req.params.id
   url = 'http://c1235100.ngrok.io/Trans/' + videoId
-  
+
   var xhr = new XMLHttpRequest()
 
   console.log("subtitle request!")
@@ -29,18 +47,18 @@ router.get('/trans/:id', function(req, res){
 
   var newVideo = Video.create({
     url: videoUrl
-  }).then(()=>{
+  }).then(() => {
     console.log("Saved video successfully")
   })
 
   subInfo = JSON.parse(xhr.responseText)
   subInfo.forEach(element => {
     SentenceBlock.create({
-          video_id: newVideo.id,
-          raw_eng: element.text,
-          start_time: element.start,
-          duration: element.duration 
-      })
+      video_id: newVideo.id,
+      raw_eng: element.text,
+      start_time: element.start,
+      duration: element.duration
+    })
   })
 
 })
